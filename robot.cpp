9abaa8fe0a7b37f1e75cd75f65d62d92875293da
Cameron Graybill <csg58@drexel.edu>
2018-10-27 10:47:17 -0400

    Updated project name

--- robot.cpp
+++ robot.cpp
@@ -2,6 +2,7 @@
 
 int main() {
     std::cout << "WE ARE DART!" << std::endl;
-	std::cout << "AND WE HAVE CI" << std::endl;
+    std::cout << "AND WE HAVE CI" << std::endl;
+    std::cout << "AND WE HAVE BULLSEYE!" << std::endl;
     return 0;
 }
