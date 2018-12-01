6eeabbc34ead591bdc09784091803db472257b0c
Jose Arguelles <j02346@gmail.com>
2018-12-01 12:18:47 -0500

    #6 Finish up ServoController

--- include/controllers/ServoController.h
+++ include/controllers/ServoController.h
@@ -7,12 +7,22 @@
 #include <drivers/AbstractPwmDriver.h>
 #include "AbstractController.h"
 
+#define FREQ 60
+
+#define VERT_PIN 8
+#define HORZ_PIN 9
+
+#define LOWER 140
+#define MID 332
+#define UPPER 540
+
 class ServoController: public ServoController {
 public:
 	ServoController(AbstractPwmDriver* pwm_driver);
 	void execute(ServoCommand* cmd);
+	double setValue(double value); 
 private:
-	AbstractPwmDriver* pwm;
+	AbstractPwmDriver* _pwmDriver;
 }
 
 	
--- src/controllers/ServoController.cpp
+++ src/controllers/ServoController.cpp
@@ -2,10 +2,34 @@
 #include <drivers/Adafruit16ChPwmHatDriver.h>
 
 ServoController::ServoController(AbstractPwmDriver* pwm_driver) {
-	pwm = pwm_driver; 
+	_pwmDriver = pwm_driver; 
 }
 
 ServoController::execute(ServoCommand* cmd) {
 	double pos = cmd->position;
+	_pwmDriver->setFrequency(FREQ);
+	_pwmDriver->setDutyCycle(VERT_PIN, self.setValue(pos)); 
+}
 
+ServoController::setValue(double value){
+	double outVal = 0; 
+
+	if(value > 1 || value < -1){
+		throw std::invalid_argument("Value out of bounds, [-1, 1]"); 
+	}
+
+	/*==Scale the input using a piecewise function===
+	 0 = mid
+	 then  0 -> 1 scale linearly to mid->upper
+	 then - 1 -> 0 scale linearly to lower->mid
+	*/
+
+	if (value > 0){
+		outVal = (value * (UPPER - MID)) + MID
+	}
+	else {
+		outVal = (value * (MID - LOWER)) + MID;
+	}
+
+	return outVal; 
 }
