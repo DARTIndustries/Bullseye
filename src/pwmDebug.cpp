5741c6c0b2f33e474b5f831f1fb5b8cd2efc782a
Gurleen Singh <gurleen@twitnit.me>
2019-01-12 13:24:03 -0500

    Added pwmDebug, updated CMakeLists

---
+++ src/pwmDebug.cpp
@@ -0,0 +1,24 @@
+// pwmDebug.cpp
+
+#include <iostream>
+#include <drivers/AbstractPwmDriver.h>
+#include <drivers/Adafruit16ChPwmHatDriver.h>
+
+int main() {
+    int pin;
+    double val;
+    std::cout << "Enter pin: ";
+    std::cin >> pin;
+    auto pwmDriver = Adafruit16ChPwmHatDriver(1, 0x40); // TODO: Make the 1 a macro
+    pwmDriver.setFrequency(60);
+    while(true) {
+        std::cout << "Enter duty cycle: ";
+        std::cin >> val;
+        if (val < -1 || val > 1) {
+            std::cout << "Error! val out of bounds!";
+            continue;
+        }
+	    pwmDriver.setDutyCycle(pin, val);
+    }
+    return 0;
+}
