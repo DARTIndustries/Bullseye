91c30e1a3cb0f190edb9b812fa4c589257b57a89
Gurleen Singh <gurleen@twitnit.me>
2019-01-14 13:25:05 -0500

    Created files for MockPwmDriver

---
+++ src/drivers/MockPwmDriver.cpp
@@ -0,0 +1,18 @@
+// Mock PWM Driver
+// 1/14/2019
+// DART
+// author: Gurleen
+
+#include <drivers/MockPwmDriver.h>
+
+MockPwmDriver::MockPwmDriver(int bus, int frequency) {
+
+}
+
+void MockPwmDriver::setFrequency(int frequency) {
+
+}
+
+void MockPwmDriver::setDutyCycle(int port, double percentDutyCycle) {
+    
+}
