3fe1e4155f87445343b232d8033024681494679a
Adam Feldscher <aff39@drexel.edu>
2019-03-16 13:55:03 -0400

    Adding fork tester

--- SharedMemory/SharedMemoryTest.cpp
+++ SharedMemory/SharedMemoryTest.cpp
@@ -1,25 +1,48 @@
 #include <iostream> 
 #include <stdio.h>
-
+#include <sys/types.h> 
 #include <SharedMemoryDriver.h>
-
+#include <unistd.h> 
 
 int main (int argc, char* argv[]) {
-
-    printf("Sharedmem startup");
     SharedMemoryDriver shm_driver;
 
-    volatile location_shm_t* loc_shm = shm_driver.open_location_shm();
+    printf("Sharedmem startup");
 
-    printf("Vect_id: %i\n", loc_shm->position_vect_id);
-    printf("vect[0]: %f\n", loc_shm->position_vect[0]);
-    printf("vect[1]: %f\n", loc_shm->position_vect[1]);
-    printf("vect[2]: %f\n", loc_shm->position_vect[2]);
+    if (fork() == 0) {
+        /* Child (gyro: read position, write motor) */
+        printf("Child fork opening shm\n");
+        volatile location_shm_t* loc_shm = shm_driver.open_location_shm();
 
-    loc_shm->position_vect_id = 0;
-    loc_shm->position_vect[0] = 1;
-    loc_shm->position_vect[1] = 2;
-    loc_shm->position_vect[2] = 3;
+        for(int i = 0; i < 50; i++) {
+            printf("child|vect_id: %i\n", loc_shm->position_vect_id);
+            printf("child|vect[0]: %f\n", loc_shm->position_vect[0]);
+            printf("child|vect[1]: %f\n", loc_shm->position_vect[1]);
+            printf("child|vect[2]: %f\n", loc_shm->position_vect[2]);
+
+            loc_shm->motor_values_id = position_vect_id;
+            loc_shm->motor_values[0] = loc_shm->position_vect[0];
+            loc_shm->motor_values[1] = loc_shm->position_vect[1];
+            loc_shm->motor_values[2] = loc_shm->position_vect[2];
+        }
+
+    } else {
+        /* Parent (robot: write position, read read motor) */
+        printf("Parent fork opening shm\n");
+        volatile location_shm_t* loc_shm = shm_driver.open_location_shm();
+
+        for(int i = 0; i < 50; i++) {
+            loc_shm->position_vect_id = i;
+            loc_shm->position_vect[0] = i + 1;
+            loc_shm->position_vect[1] = i + 2;
+            loc_shm->position_vect[2] = i + 3;
+
+            printf("parent|motor_id: %i\n", loc_shm->motor_values_id);
+            printf("parent|motor[0]: %f\n", loc_shm->motor_values[0]);
+            printf("parent|motor[1]: %f\n", loc_shm->motor_values[1]);
+            printf("parent|motor[2]: %f\n", loc_shm->motor_values[2]);
+        }
+    }
 
     return 0;
 }
